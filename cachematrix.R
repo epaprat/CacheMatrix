26768994c6a99ced9a86ec0f421395820b25957a
Ertaç Paprat <epaprat@gmail.com>
2014-04-29 00:38:07 +0300

    makeCacheMatrix and cacheSolve implemented
    R Programming Week 3 Peer Assessment Problem Solution.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Simple calculation, caching mechanism for the Matrix Inversions
 
-## Write a short comment describing this function
+## Data Structure for the Matrix and its Inverse. It simply gets a matrix as an 
+## parameter and encapsulate it with its inverse and provide setter, getter
+## options.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        i <- NULL
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        get <- function() x
+        setInverse <- function(inverse) i <<- inverse
+        getInverse <- function() i
+        list(set = set, get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## It gets makeCacheMatrix type data structure as a parameter and returns 
+## inversion of the matrix which encaplsutated with it. It returns inverse 
+## matrix as is if it's already exists inside the makeCacheMatrix, if not it
+## calculates and set it inside makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+        i <- x$getInverse()
+        if(!is.null(i)) {
+                message("getting cached data")
+                return(i)
+        }
+        data <- x$get()
+        i <- solve(data, ...)
+        x$setInverse(i)
+        i
 }
